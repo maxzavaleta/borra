7f8492e4ceeb0b59b5c24fa7cd16ef69d27401db
Max Zavaleta Villar <mzavaletav@outlook.com>
2020-10-03 11:18:52 -0500

    conf.ev add

--- Program.cs
+++ Program.cs
@@ -13,15 +13,8 @@ namespace sovos1
     class Program
     {
         const string URL = "http://spsa.paperless.com.pe/axis2/services/Online.OnlineHttpSoap11Endpoint";
-        //static string ruc;
-        static string login;
-        static string clave;
         static void Main(string[] args)
         {
-            //ruc="20536557858"; //HP
-            //ruc="20394077101"; //HPO
-            login="admin_hpo";
-            clave="abc123";
 
             String sfolios = File.ReadAllText("data.in");
             string [] folios= sfolios.Split("\r\n");
@@ -31,26 +24,44 @@ namespace sovos1
 
                 if (item!=""){
                     string [] sLine = item.Split("\t");
-                    
-                    string message = recovery(sLine[1], sLine[0]);
+                    string ruc = sLine[1];
 
-                    if (message.StartsWith("Respuesta SUNAT: 2028 - 2028") 
-                        || message.StartsWith("Respuesta SUNAT: 3127 - 3127")
-                        || message.StartsWith("Respuesta SUNAT: 3031 - 3031") 
-                        || message.StartsWith("Respuesta SUNAT: 3035 - 3035")
-                        || message.StartsWith("Respuesta SUNAT: 3202 - ")
-                    ){
-                        //Console.WriteLine("Procesar "+ item);
-                        generation(sLine[0], sLine[1]);
+                    string [] conf = getConf(ruc);
+
+                    if (conf!=null){
+                        if(conf.Length==4){
+                            string message = recovery(ruc, sLine[0],conf[1],conf[2]);
+                            if (message.StartsWith("Respuesta SUNAT: 2028 - 2028") 
+                                || message.StartsWith("Respuesta SUNAT: 3127 - 3127")
+                                || message.StartsWith("Respuesta SUNAT: 3031 - 3031") 
+                                || message.StartsWith("Respuesta SUNAT: 3035 - 3035")
+                                || message.StartsWith("Respuesta SUNAT: 3202 - ")
+                                || message.StartsWith("Documento no encontrado, RUC ")
+                            ){
+                                generation(sLine[0], sLine[1],conf[1],conf[2],conf[3]);
+                            }                            
+                        }
                     }
-
                 }
-                
                 //
             }
         }
+        static string[] getConf(string ruc){
+            String content = File.ReadAllText("conf.env");
+            string [] lines= content.Split("\r\n");
+            foreach (var line in lines)
+            {
+                if(!line.StartsWith("#")){
+                    string [] data = line.Split("|");
+                    if(data[0].Equals(ruc)){
+                        return data;
+                    }
+                }
+            }
+            return null;
+        }
 
-        static String recovery(string ruc,string folio){
+        static String recovery(string ruc,string folio, string login, string clave){
 
             var tipoDoc="1";
             //var folio="FA01-00447663";
@@ -79,16 +90,24 @@ namespace sovos1
             dataXmlRet = Regex.Replace(dataXmlRet, "&(?!(amp|apos|quot|lt|gt);)", "&amp;");
 
             XDocument docd = XDocument.Parse(dataXmlRet);
+
+            string message=docd.Element("Respuesta").Element("Mensaje").Value;
+            int i  =message.IndexOf("\n");
+            if(i>0){
+                message = message.Substring(0,i);
+            }
+            //Console.WriteLine(i);
+
             Console.WriteLine("Folio {0}: Status: {1} - {2}",
                 folio,
                 docd.Element("Respuesta").Element("Codigo").Value,
-                docd.Element("Respuesta").Element("Mensaje").Value);
+                message);
             return docd.Element("Respuesta").Element("Mensaje").Value;
         }
-        static void generation(string folio, string ruc){
+        static void generation(string folio, string ruc,string login, string clave, string conString){
 
             string dataXml = File.ReadAllText( "generation.xml");
-            string dataText = getData(folio,ruc);
+            string dataText = getData(folio,ruc,conString);
             if(dataText!=""){
                 dataXml=dataXml.Replace("@@ruc@@",ruc);
                 dataXml=dataXml.Replace("@@login@@",login);
@@ -116,8 +135,9 @@ namespace sovos1
             }
         }
 
-        static String getData(String folio, String ruc){
-            String conString = "user id=ecthp;Password=SE1010;Data Source=10.20.11.11:1542/hpt01";
+        static String getData(String folio, String ruc, string conString){
+            //String conString = "user id=ecthp;Password=SE1010;Data Source=10.20.11.11:1542/hpt01";
+            //String conString = "user id=tiendas_adm;Password=tdatpsa;Data Source=10.20.11.21:1525/pmm";
             String retData="";
             using(OracleConnection cn = new OracleConnection(conString)){
                 cn.Open();
